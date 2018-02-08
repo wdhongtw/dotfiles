ab6d40cb7706782bfa49a90f22259aa9e7b50d24
Weida Hong <wdhongtw@gmail.com>
2018-02-08 14:02:44 +0800

    Add ability to custom install directory

--- install.sh
+++ install.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
-DOTFILES_DIR="$HOME/.dotfiles"
+DOTFILES_DIR=${DOTFILES_DIR:-"$HOME/.dotfiles"}
+DEST_HOME=${DEST_HOME:-"$HOME"}
 cd "$DOTFILES_DIR" || exit
 
 FILE_LIST=".screenrc .tmux.conf .vimrc .vimrc.d .bashrc.d"
 install() {
   printf "Install dotfiles...\n"
   for FILE in $FILE_LIST; do
-    rsync -avh --no-perms "$FILE" "$HOME";
+    rsync -avh --no-perms "$FILE" "$DEST_HOME";
   done
   printf "Install completes.\n"
   printf "Create folders for vim temporary files in \$HOME/.vim.\n"
   for FOLDER in undo swap backup; do
-    mkdir -p -v "$HOME/.vim/$FOLDER"
+    mkdir -p -v "$DEST_HOME/.vim/$FOLDER"
   done
 }
 
--- softlink.sh
+++ softlink.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
+DEST_HOME=${DEST_HOME:-"$HOME"}
 cd "$(dirname "$0")" || exit
 
 FILE_LIST=".screenrc .tmux.conf .vimrc .vimrc.d .bashrc.d"
 install() {
   printf "Link dotfiles...\n"
   for FILE in $FILE_LIST; do
-    ln -s -v "$(pwd)/$FILE" "$HOME";
+    ln -s -v "$(pwd)/$FILE" "$DEST_HOME";
   done
   printf "Link completes.\n"
   printf "Create folders for vim temporary files in \$HOME/.vim.\n"
   for FOLDER in undo swap backup; do
-    mkdir -p -v "$HOME/.vim/$FOLDER"
+    mkdir -p -v "$DEST_HOME/.vim/$FOLDER"
   done
 }
 
